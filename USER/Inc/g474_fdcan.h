a156d6ec2e7f9ea3b2b120d13aeb7433bcc40bdc
Rian <rn1494278048@outlook.com>
2025-03-20 23:11:22 +0800

    25/3/20/01

--- USER/Inc/g474_fdcan.h
+++ USER/Inc/g474_fdcan.h
@@ -9,6 +9,8 @@
 
 void fdcan2_config(void);
 void send_capinfo();
+uint8_t Get_CRC8_Check_Sum(unsigned char *pchMessage, unsigned int dwLength);
+uint16_t Get_CRC16_Check_Sum(unsigned char *pchMessage, unsigned int dwLength);
 
 
 
